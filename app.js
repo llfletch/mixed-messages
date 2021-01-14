2480f797517215069d76fbac8550369e0b6256ff
Loren <lorenfletcher@msn.com>
2021-01-13 22:04:32 -0700

    add scriptural references

--- app.js
+++ app.js
@@ -1,7 +1,66 @@
 let topics = 
     [ { topic: "Jesus Christ"
-        , keyWords: "Jesus Christ savior redeemer" },
+        , keyWords: "Jesus Christ savior redeemer Son" },
       { topic: "atonement", keyWords: ""} ];
-let bookOfMormon = [ { quote: "", author: "" } ];
-let doctrineAndCovenants = [ { quote: "", author: "" } ];
-console.log("The message is: ...");
+let bookOfMormon = [ 
+    { ref: "Moroni 10:5"
+    , quote: "And by the power of the Holy Ghost ye may know the truth of all things." } 
+    , { ref: "Moroni 10:7"
+    , quote: "And ye may know that he is, by the power of the Holy Ghost; wherefore I would exhort you that ye deny not the power of God; for he worketh by power, according to the faith of the children of men, the same today and tomorrow, and forever." }  
+    , { ref: "Moroni 10:20"
+    , quote: "Wherefore, there must be faith; and if there must be faith there must also be hope; and if there must be hope there must also be charity." }  
+    , { ref: "Moroni 10:22"
+    , quote: "And if ye have no hope ye must needs be in despair; and despair cometh because of iniquity." }  
+    , { ref: "Moroni 10:23"
+    , quote: "And Christ truly said unto our fathers: If ye have faith ye can do all things which are expedient unto me." }  
+    , { ref: ""
+    , quote: "" }  
+    , { ref: ""
+    , quote: "" }  
+    , { ref: ""
+    , quote: "" }  
+    , { ref: ""
+    , quote: "" }  
+    , { ref: ""
+    , quote: "" }  
+    , { ref: ""
+    , quote: "" }  
+    , { ref: ""
+    , quote: "" }  
+    ];
+let doctrineAndCovenants = [
+     { ref: "82:10"
+       , quote: "I, the Lord, am bound when ye do what I say; but when ye do not what I say, ye have no promise." }
+     , { ref: "82:3"
+       , quote: "For of him unto whom much is given much is required; and he who sins against the greater light shall receive the greater condemnation." }
+     , { ref: "122:7-8"
+       , quote: "...and above all, if the very jaws of hell shall gape open the mouth wide after thee, know thou, my son, that all these things shall give thee experience, and shall be for thy good. The Son of Man hath descended below them all. Art thou greater than he?" }
+     , { ref: "109:7"
+       , quote: "And as all have not faith, seek ye diligently and teach one another words of wisdom; yea, seek ye out of the best books words of wisdom, seek learning even by study and also by faith;" }
+     , { section: "93:1" 
+       , quote: "Verily, thus saith the Lord: It shall come to pass that every soul who forsaketh his sins and cometh unto me, and calleth on my name, and obeyeth my voice, and keepeth my commandments, shall see my face and know that I am;" }
+     , { ref: "93:24" 
+       , quote: "And truth is knowledge of things as they are, and as they were, and as they are to come;" }
+     , { ref: "93:28" 
+       , quote: "He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things." }
+       , { ref: "93:33" 
+       , quote: "For man is spirit. The elements are eternal, and spirit and element, inseparably connected, receive a fulness of joy;" }    
+       , { ref: "93:36" 
+       , quote: "The glory of God is intelligence, or, in other words, light and truth." }    
+       , { ref: "93:40" 
+       , quote: "But I have commanded you to bring up your children in light and truth." }    
+       , { ref: "90:24" 
+       , quote: "Search diligently, pray always, and be believing, and all things shall work together for your good, if ye walk uprightly and remember the covenant wherewith ye have covenanted one with another." }    
+       , { ref: "90:17" 
+       , quote: "Be not ashamed, neither confounded; but be admonished in all your high-mindedness and pride, for it bringeth a snare upon your souls." }    
+       , { ref: "90:18" 
+       , quote: "Set in order your houses; keep slothfulness and uncleanness far from you." }    
+       , { ref: ":" 
+       , quote: "" }    
+       , { ref: ":" 
+       , quote: "" }    
+       , { ref: ":" 
+       , quote: "" }    
+    ];
+
+       console.log("The message is: ...");
