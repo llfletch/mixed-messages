04a6976780685c43e5ca2ae7b09494874ffe6142
Loren <lorenfletcher@msn.com>
2021-01-13 18:59:21 -0700

    add structure for messages

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+let topics = 
+    [ { topic: "Jesus Christ"
+        , keyWords: "Jesus Christ savior redeemer" },
+      { topic: "atonement", keyWords: ""} ];
+let bookOfMormon = [ { quote: "", author: "" } ];
+let doctrineAndCovenants = [ { quote: "", author: "" } ];
 console.log("The message is: ...");
